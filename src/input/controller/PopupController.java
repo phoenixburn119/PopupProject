92ab836017b839110441554014932ba0db722892
Adam Kinzer <adamkinzer915@gmail.com>
2015-09-24 13:49:44 -0600

    Added toString and popup questions.

--- src/input/controller/PopupController.java
+++ src/input/controller/PopupController.java
@@ -1,10 +1,83 @@
 package input.controller;
 
+import input.view.PopupDisplay;
+import input.model.Thing;
+
 public class PopupController
 {
+	private PopupDisplay myPopups;
+	private Thing myThing;
+	
+	public PopupController()
+	{
+		myPopups = new PopupDisplay();
+	}
 	
 	public void start()
 	{
+		String name = myPopups.getAnswer("Type in your name");
 		
+		myPopups.displayResponse("Thank you, " + name);
+		
+		int age;
+		String tempAge = myPopups.getAnswer("Type in your age");
+		if(isInteger(tempAge))
+		{
+			age = Integer.parseInt(tempAge);
+		}
+		else
+		{
+			age = 16;
+		}
+		
+		myPopups.displayResponse("You are " + age);
+		
+		double weight;
+		String tempWeight = myPopups.getAnswer("Type in your weight");
+		if(isDouble(tempWeight))
+		{
+			weight = Double.parseDouble(tempWeight);
+		}
+		else
+		{
+			weight = 120;
+		}
+		myPopups.displayResponse("You typed in " + weight);
+		
+		myThing = new Thing(name, age, weight);
+		
+		myPopups.displayResponse(myThing.toString());
+	}
+	
+	private boolean isInteger(String input)
+	{
+		boolean isInt = false;
+		
+		try
+		{
+			int validInteger = Integer.parseInt(input);
+			isInt = true;
+		}
+		catch(NumberFormatException error)
+		{
+			myPopups.displayResponse("You did not type in a valid integer.");
+		}
+		return isInt;
+	}
+	
+	private boolean isDouble(String input)
+	{
+		boolean isDouble = false;
+		
+		try
+		{
+			double validDouble = Double.parseDouble(input);
+			isDouble = true;
+		}
+		catch(NumberFormatException error)
+		{
+			myPopups.displayResponse("You did not type in a valid number.");
+		}
+		return isDouble;
 	}
 }
---
+++ src/input/model/Thing.java
@@ -0,0 +1,28 @@
+package input.model;
+
+public class Thing
+{
+	private String name;
+	private int age;
+	private double weight;
+	
+	public Thing()
+	{
+		age = -99;
+		weight = -.05;
+		name = "";
+	}
+	
+	public Thing(String name, int age, double weight)
+	{
+		this.age = age;
+		this.name = name;
+		this.weight = weight;
+	}
+	
+	public String toString()
+	{
+		String thingInfo = " name: " + name + " age: " + age + " weight: " + weight;
+		return thingInfo;
+	}
+}
